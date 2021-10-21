89fb658fcbd3d21cb4e281ee8fcb4bf5aef3f824
dvdson <dvdsoncomp@gmail.com>
2021-10-21 18:46:11 -0300

    Adição de elementos gui
    Screens "Game Over", "Settings" e "Circle Jump" adicionandos, e
    funcionando.
    
    Ao perder o jogador é levado a tela "Game Over", Além de pode navegar
    entre:
     - "Game Over" -> "Circle Jump"
     - "Game" -> "Game Over"
     - "Circle Jump" -> "Settings"
     - "Settings" -> "Circle Jump"
     - "Circle Jump" -> "Game"

--- scripts/Jumper/Jumper.gd
+++ scripts/Jumper/Jumper.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal captured
+signal died
 
 var velocity = Vector2(100, 0) 
 var jump_speed = 1000
@@ -24,24 +25,25 @@ func die():
 	
 func _on_VisibilityNotifier2D_screen_exited():
 	if !target:
+		emit_signal("died")
 		die()
 	
 func _on_Jumper_area_entered(area):
 	target = area
-	velocity = Vector2()
+	velocity = Vector2.ZERO
 	emit_signal("captured", area)
 	# target.get_node("Pivot").rotation = (position - target.position).angle()
 
 
 func _physics_process(delta):
+	if trail.points.size() > trail_length:
+		trail.remove_point(0)
+	trail.add_point(position)
+	
 	if target:
 		transform = target.orbit_position.global_transform
 	else:
 		position += velocity * delta
-	
-	if trail.points.size() > trail_length:
-		trail.remove_point(0)
-	trail.add_point(position)
 
 
 
